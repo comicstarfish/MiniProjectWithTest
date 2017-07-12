cdd8036c85a9d4429b04f0d87ef038979ca6e3d4
MANOJ <MANOJ@MANOJ-NOTEBOOK>
2017-07-12 14:41:23 +0530

    hi

---
+++ src/paintcostestimator/PaintCostEstimator.java
@@ -0,0 +1,17 @@
+
+package paintcostestimator;
+
+/**
+ *
+ * @author MANOJ
+ */
+public class PaintCostEstimator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

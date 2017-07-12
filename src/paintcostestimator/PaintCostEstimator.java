dc932ab726545be071b0af084b507ff557acf013
MANOJ <MANOJ@MANOJ-NOTEBOOK>
2017-07-12 14:59:08 +0530

    test

--- src/paintcostestimator/PaintCostEstimator.java
+++ src/paintcostestimator/PaintCostEstimator.java
@@ -12,6 +12,7 @@ public class PaintCostEstimator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Hi nikita, hi Sagar!");
     }
     
 }
---
+++ test/Testing.java
@@ -0,0 +1,42 @@
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import paintcostestimator.PaintCostEstimator;
+
+/**
+ *
+ * @author MANOJ
+ */
+public class Testing {
+    PaintCostEstimator test;
+    
+    public Testing() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        test=new PaintCostEstimator();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
